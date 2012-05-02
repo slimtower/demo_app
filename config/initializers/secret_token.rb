230c82dd7f47ab1536b495ba07cc5b74f9e53cc5
slimtower <slimtower@gmail.com>
2012-05-02 14:50:05 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'dd07c797101eb1eb125c1999a99970cc8157c003d8b76f9926d847798e79a9ee52c7de696289e062ebf4ccec98d9aeae6106d0c08d0b5cdc0bbb8a8167031f8a'
